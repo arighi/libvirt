9c0fe34dbc91cace24f007a5f59b3e2085dedeb4
Andrea Righi <arighi@nvidia.com>
2025-02-19 17:59:42 +0100

    conf: Introduce acpi-generic-initiator device
    Introduce apci-generic-initiator device to the domain XML.
    
    Example definition:
    
      <acpi-generic-initiator>
        <pci-dev>dev0</pci-dev>
        <numa-node>1</numa-node>
      </acpi-generic-initiator>
    
    This enables partitioning of PCI resources into multiple isolated
    instances, each requiring a dedicated NUMA node definition, that can be
    represented by the acpi-generic-initiator object.
    
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- src/qemu/qemu_alias.c
+++ src/qemu/qemu_alias.c
@@ -665,6 +665,14 @@ qemuAssignDevicePstoreAlias(virDomainPstoreDef *pstore)
         pstore->info.alias = g_strdup("pstore0");
 }
 
+static void
+qemuAssignDeviceAcpiInitiatorAlias(virDomainAcpiInitiatorDef *acpiinitiator,
+                                   int idx)
+{
+    if (!acpiinitiator->info.alias)
+        acpiinitiator->info.alias = g_strdup_printf("gi%d", idx);
+}
+
 
 int
 qemuAssignDeviceAliases(virDomainDef *def)
@@ -757,6 +765,9 @@ qemuAssignDeviceAliases(virDomainDef *def)
     }
     if (def->pstore)
         qemuAssignDevicePstoreAlias(def->pstore);
+    for (i = 0; i < def->nacpiinitiator; i++) {
+        qemuAssignDeviceAcpiInitiatorAlias(def->acpiinitiator[i], i);
+    }
 
     return 0;
 }
